cd3de7f31a414114879a2f180601d7eeccd4eae2
Benjamin Kleynhans <bxk8027@rit.edu>
2020-03-01 13:03:47 -0500

    Due to the way MySQL was implemented I had trouble making it work with Vue.  I just removed the last references to the Vue elements in the code

--- src/jsResponses.js
+++ src/jsResponses.js
@@ -1,10 +1,9 @@
 const fs = require('fs'); // pull in the file system module
 
 const bundle = fs.readFileSync(`${__dirname}/../hosted/bundle.js`);
-// const vueMaster = fs.readFileSync(`${__dirname}/../hosted/vueMaster.js`);
 const gpaPageManager = fs.readFileSync(`${__dirname}/../src/gpaPageManager.js`);
 
-// Return the vue components
+// Return the JavaScript page manager which manages the entire GPA page (lol)
 const getGpaPageManager = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'application/javascript' });
   response.write(gpaPageManager);
@@ -18,15 +17,8 @@ const getBundle = (request, response) => {
   response.end();
 };
 
-// // Return the JavaScript Vue master
-// const getVueMaster = (request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/javascript' });
-//   response.write(vueMaster);
-//   response.end();
-// };
-
+// Export locally defined functions for external use
 module.exports = {
   getBundle,
-  // getVueMaster,
   getGpaPageManager,
 };
